3c61cd4be0ba78e207a88cfabd3dd3c2ca1cc735
YuanShuoHsu <ook1725@gmail.com>
2025-06-04 20:43:34 +0800

    fix: 在 CreateEcpayDto 中新增 @IsNotEmptyObject 裝飾器以強化欄位驗證，確保 base 和 invoice 物件不為空。

--- src/ecpay/dto/create-ecpay.dto.ts
+++ src/ecpay/dto/create-ecpay.dto.ts
@@ -1,17 +1,19 @@
 // src/ecpay/dto/create-ecpay.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { ValidateNested } from 'class-validator';
+import { IsNotEmptyObject, ValidateNested } from 'class-validator';
 import { BaseEcpayDto } from './base-ecpay.dto';
 import { InvoiceEcpayDto } from './invoice-ecpay.dto';
 
 export class CreateEcpayDto {
   @ApiProperty({ type: BaseEcpayDto })
+  @IsNotEmptyObject()
   @ValidateNested()
   @Type(() => BaseEcpayDto)
   base: BaseEcpayDto;
 
   @ApiProperty({ type: InvoiceEcpayDto })
+  @IsNotEmptyObject()
   @ValidateNested()
   @Type(() => InvoiceEcpayDto)
   invoice: InvoiceEcpayDto;
