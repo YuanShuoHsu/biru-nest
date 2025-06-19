a655d4a6679b16bb43de82b35f6be95ad0d03b02
YuanShuoHsu <ook1725@gmail.com>
2025-06-19 22:10:31 +0800

    refactor:更新 BaseEcpayDto 的屬性描述，將多行文字格式化為 HTML 列表，以提升可讀性和使用者理解度。

--- src/ecpay/dto/base-ecpay.dto.ts
+++ src/ecpay/dto/base-ecpay.dto.ts
@@ -50,9 +50,11 @@ enum BaseEcpayLanguage {
 
 export class BaseEcpayDto {
   @ApiProperty({
-    description: `特店編號
-    測試環境特店編號
-    正式環境金鑰取得`,
+    description: `特店編號（必填）
+    <ul>
+    <li>測試環境特店編號</li>
+    <li>正式環境金鑰取得</li>
+    </ul>`,
     example: '3002607',
   })
   @IsDefined()
@@ -62,9 +64,11 @@ export class BaseEcpayDto {
   MerchantID: string;
 
   @ApiProperty({
-    description: `特店訂單編號
-    特店訂單編號均為唯一值，不可重複使用。
-    英數字大小寫混合`,
+    description: `特店訂單編號（必填）
+    <ul>
+    <li>特店訂單編號均為唯一值，不可重複使用。</li>
+    <li>英數字大小寫混合</li>
+    </ul>`,
     example: 'ORD20250607ABCDE1234',
   })
   @IsDefined()
@@ -74,7 +78,7 @@ export class BaseEcpayDto {
   MerchantTradeNo: string;
 
   @ApiProperty({
-    description: `特店交易時間
+    description: `特店交易時間（必填）<br>
     格式為：yyyy/MM/dd HH:mm:ss`,
     example: '2025/06/07 15:00:00',
   })
@@ -86,7 +90,7 @@ export class BaseEcpayDto {
   MerchantTradeDate: string;
 
   @ApiProperty({
-    description: `交易類型
+    description: `交易類型（必填）<br>
     請固定填入 aio`,
     enum: BaseEcpayPaymentType,
     example: BaseEcpayPaymentType.AIO,
@@ -99,9 +103,11 @@ export class BaseEcpayDto {
   PaymentType: BaseEcpayPaymentType;
 
   @ApiProperty({
-    description: `交易金額
-    請帶整數，不可有小數點。
-    僅限新台幣`,
+    description: `交易金額（必填）
+    <ul>
+    <li>請帶整數，不可有小數點。</li>
+    <li>僅限新台幣</li>
+    </ul>`,
     example: 100,
   })
   @IsDefined()
@@ -109,7 +115,7 @@ export class BaseEcpayDto {
   TotalAmount: number;
 
   @ApiProperty({
-    description: `交易描述
+    description: `交易描述（必填）<br>
     請勿帶入特殊字元。`,
     example: '商品訂購',
   })
@@ -120,9 +126,11 @@ export class BaseEcpayDto {
   TradeDesc: string;
 
   @ApiProperty({
-    description: `商品名稱
-    如果商品名稱有多筆，需在金流選擇頁一行一行顯示商品名稱的話，商品名稱請以符號 # 分隔。
-    商品名稱字數限制為中英數 400 字內，超過此限制系統將自動截斷。 詳細的使用注意事項請參考 FAQ。`,
+    description: `商品名稱（必填）
+    <ul>
+    <li>如果商品名稱有多筆，需在金流選擇頁一行一行顯示商品名稱的話，商品名稱請以符號 # 分隔。</li>
+    <li>商品名稱字數限制為中英數 400 字內，超過此限制系統將自動截斷。 詳細的使用注意事項請參考 FAQ。</li>
+    </ul>`,
     example: '商品A#商品B',
   })
   @IsDefined()
@@ -132,14 +140,18 @@ export class BaseEcpayDto {
   ItemName: string;
 
   @ApiProperty({
-    description: `付款完成通知回傳網址
-    ReturnURL 為付款結果通知回傳網址，為特店 server 或主機的 URL，用來接收綠界後端回傳的付款結果通知。
-    當消費者付款完成後，綠界會將付款結果參數以幕後回傳到該網址。詳細說明請參考付款結果通知
+    description: `付款完成通知回傳網址（必填）
+    <ul>
+    <li>ReturnURL 為付款結果通知回傳網址，為特店 server 或主機的 URL，用來接收綠界後端回傳的付款結果通知。</li>
+    <li>當消費者付款完成後，綠界會將付款結果參數以幕後回傳到該網址。詳細說明請參考付款結果通知</li>
+    </ul>
     注意事項：
-    請勿設定與 Client 端接收付款結果網址 OrderResultURL 相同位置，避免程式判斷錯誤。
-    ReturnURL 收到綠界後端回傳的付款結果通知後，請回應字串 1|OK 給綠界。
-    1|OK 僅是廠商回應綠界是否收到通知，並不會改變付款狀態。
-    參數內容若有包含 %26(&) 及 %3C(<) 這二個值時，請先進行 urldecode() 避免呼叫 API 失敗。`,
+    <ol>
+    <li>請勿設定與 Client 端接收付款結果網址 OrderResultURL 相同位置，避免程式判斷錯誤。</li>
+    <li>ReturnURL 收到綠界後端回傳的付款結果通知後，請回應字串 1|OK 給綠界。</li>
+    <li>1|OK 僅是廠商回應綠界是否收到通知，並不會改變付款狀態。</li>
+    <li>參數內容若有包含 %26(&) 及 %3C(<) 這二個值時，請先進行 urldecode() 避免呼叫 API 失敗。</li>
+    </ol>`,
     example: 'https://your-domain.com/api/ecpay/notify',
   })
   @IsDefined()
@@ -149,22 +161,26 @@ export class BaseEcpayDto {
   ReturnURL: string;
 
   @ApiProperty({
-    description: `選擇預設付款方式
+    description: `選擇預設付款方式（必填）<br>
     綠界提供下列付款方式：
-    Credit：信用卡及銀聯卡（需申請開通）
-    TWQR ：歐付寶 TWQR 行動支付（需申請開通）
-    WebATM：網路ATM
-    ATM：自動櫃員機
-    CVS：超商代碼
-    BARCODE：超商條碼
-    ApplePay：Apple Pay（僅支援手機支付）
-    BNPL：裕富無卡分期（需申請開通）
-    WeiXin：微信支付
-    ALL：不指定付款方式，由綠界顯示付款方式選擇頁面。
+    <ul>
+    <li>Credit：信用卡及銀聯卡（需申請開通）</li>
+    <li>TWQR：歐付寶 TWQR 行動支付（需申請開通）</li>
+    <li>WebATM：網路ATM</li>
+    <li>ATM：自動櫃員機</li>
+    <li>CVS：超商代碼</li>
+    <li>BARCODE：超商條碼</li>
+    <li>ApplePay：Apple Pay（僅支援手機支付）</li>
+    <li>BNPL：裕富無卡分期（需申請開通）</li>
+    <li>WeiXin：微信支付</li>
+    <li>ALL：不指定付款方式，由綠界顯示付款方式選擇頁面。</li>
+    </ul>
     注意事項：
-    若為手機版時不支援下列付款方式：WebATM
-    如需要不透過綠界畫面取得 ATM、CVS、BARCODE 的繳費代碼，請參考如何不經過綠界畫面取得 ATM、CVS、BARCODE 的繳費代碼。
-    當瀏覽器不為 Safari 時，不會顯示 Apple Pay 付款功能。`,
+    <ol>
+    <li>若為手機版時不支援下列付款方式：WebATM</li>
+    <li>如需要不透過綠界畫面取得 ATM、CVS、BARCODE 的繳費代碼，請參考如何不經過綠界畫面取得 ATM、CVS、BARCODE 的繳費代碼。</li>
+    <li>當瀏覽器不為 Safari 時，不會顯示 Apple Pay 付款功能。</li>
+    </ol>`,
     enum: BaseEcpayChoosePayment,
     example: BaseEcpayChoosePayment.ALL,
   })
@@ -176,7 +192,7 @@ export class BaseEcpayDto {
   ChoosePayment: BaseEcpayChoosePayment;
 
   @ApiProperty({
-    description: `檢查碼
+    description: `檢查碼（必填）<br>
     請參考檢查碼機制`,
     example: '85D927637935683EA756CDEF76498FEB9F5D098A7A1AC4F0CB3B3609A9D4116A',
   })
@@ -186,7 +202,7 @@ export class BaseEcpayDto {
   CheckMacValue: string;
 
   @ApiProperty({
-    description: `CheckMacValue 加密類型
+    description: `CheckMacValue 加密類型（必填）<br>
     請固定填入 1，使用 SHA256 加密。`,
     enum: BaseEcpayEncryptType,
     example: BaseEcpayEncryptType.SHA256,
@@ -197,7 +213,7 @@ export class BaseEcpayDto {
   EncryptType: BaseEcpayEncryptType;
 
   @ApiPropertyOptional({
-    description: `特店旗下店舖代號
+    description: `特店旗下店舖代號<br>
     提供特店填入分店代號使用，僅可用英數字大小寫混合。`,
     example: 'STORE123',
   })
@@ -208,15 +224,17 @@ export class BaseEcpayDto {
   StoreID?: string;
 
   @ApiPropertyOptional({
-    description: `Client 端返回特店的按鈕連結
-    消費者點選此按鈕後，會將頁面導回到此設定的網址
+    description: `Client 端返回特店的按鈕連結<br>
+    消費者點選此按鈕後，會將頁面導回到此設定的網址<br>
     注意事項：
-    導回時不會帶付款結果到此網址，只是將頁面導回而已。
-    設定此參數，綠界會在付款完成或取號完成頁面上顯示 [返回商店] 的按鈕。
-    設定此參數，發生簡訊 OTP 驗證失敗時，頁面上會顯示 [返回商店] 的按鈕。
-    若未設定此參數，則綠界付款完成頁或取號完成頁面，不會顯示 [返回商店] 的按鈕。
-    若導回網址未使用 https 時，部份瀏覽器可能會出現警告訊息。
-    參數內容若有包含 %26(&) 及 %3C(<) 這二個值時，請先進行 urldecode() 避免呼叫 API 失敗。`,
+    <ol>
+    <li>導回時不會帶付款結果到此網址，只是將頁面導回而已。</li>
+    <li>設定此參數，綠界會在付款完成或取號完成頁面上顯示 [返回商店] 的按鈕。</li>
+    <li>設定此參數，發生簡訊 OTP 驗證失敗時，頁面上會顯示 [返回商店] 的按鈕。</li>
+    <li>若未設定此參數，則綠界付款完成頁或取號完成頁面，不會顯示 [返回商店] 的按鈕。</li>
+    <li>若導回網址未使用 https 時，部份瀏覽器可能會出現警告訊息。</li>
+    <li>參數內容若有包含 %26(&) 及 %3C(<) 這二個值時，請先進行 urldecode() 避免呼叫 API 失敗。</li>
+    </ol>`,
     example: 'https://your-domain.com/shop',
   })
   @IsOptional()
@@ -246,8 +264,8 @@ export class BaseEcpayDto {
   Remark?: string;
 
   @ApiPropertyOptional({
-    description: `付款子項目
-    若設定此參數，建立訂單將轉導至綠界訂單成立頁，依設定的付款方式及付款子項目帶入訂單，無法選擇其他付款子項目。請參考付款方式一覽表
+    description: `付款子項目<br>
+    若設定此參數，建立訂單將轉導至綠界訂單成立頁，依設定的付款方式及付款子項目帶入訂單，無法選擇其他付款子項目。請參考付款方式一覽表<br>
     注意事項：因板信銀行會於每月進行例行維護，當遇銀行維護時，將會建立訂單失敗。`,
     example: 'Visa',
   })
@@ -258,13 +276,15 @@ export class BaseEcpayDto {
   ChooseSubPayment?: string;
 
   @ApiPropertyOptional({
-    description: `Client 端回傳付款結果網址
-    有別於 ReturnURL（server端的網址），OrderResultURL 為特店的 client 端（前端）網址。消費者付款完成後，綠界會將付款結果參數以 POST 方式回傳到到該網址。詳細說明請參考付款結果通知。
+    description: `Client 端回傳付款結果網址<br>
+    有別於 ReturnURL（server端的網址），OrderResultURL 為特店的 client 端（前端）網址。消費者付款完成後，綠界會將付款結果參數以 POST 方式回傳到到該網址。詳細說明請參考付款結果通知。<br>
     注意事項：
-    若與 [ClientBackURL] 同時設定，將會以此參數為主。
-    銀聯卡及非即時交易（ATM、CVS、BARCODE）不支援此參數。
-    付款結果通知請依 ReturnURL（server端的網址）為主,避免因前端操作或網路問題未收到 OrderResultURL 特店的 client 端（前端）的通知。
-    參數內容若有包含 %26(&) 及 %3C(<) 這二個值時，請先進行 urldecode() 避免呼叫 API 失敗。`,
+    <ol>
+    <li>若與 [ClientBackURL] 同時設定，將會以此參數為主。</li>
+    <li>銀聯卡及非即時交易（ATM、CVS、BARCODE）不支援此參數。</li>
+    <li>付款結果通知請依 ReturnURL（server端的網址）為主,避免因前端操作或網路問題未收到 OrderResultURL 特店的 client 端（前端）的通知。</li>
+    <li>參數內容若有包含 %26(&) 及 %3C(<) 這二個值時，請先進行 urldecode() 避免呼叫 API 失敗。</li>
+    </ol>`,
     example: 'https://your-domain.com/ecpay/result',
   })
   @IsOptional()
@@ -274,10 +294,13 @@ export class BaseEcpayDto {
   OrderResultURL?: string;
 
   @ApiPropertyOptional({
-    description: `是否需要額外的付款資訊
+    description: `是否需要額外的付款資訊<br>
     額外的付款資訊
-    若不回傳額外的付款資訊時，參數值請傳：Ｎ；
-    若要回傳額外的付款資訊時，參數值請傳：Ｙ ，付款完成後綠界後端會以 POST 方式回傳額外付款資訊到特店的 ReturnURL 與 OrderResultURL。`,
+    <ul>
+    <li>若不回傳額外的付款資訊時，參數值請傳：Ｎ；</li>
+    <li>若要回傳額外的付款資訊時，參數值請傳：Ｙ ，付款完成後綠界後端會以 POST 方式回傳額外付款資訊到特店的 ReturnURL 與 OrderResultURL。</li>
+    </ul>
+    注意事項：回傳額外付款資訊參數請參考-額外回傳的參數`,
     enum: BaseEcpayNeedExtraPaidInfo,
     example: BaseEcpayNeedExtraPaidInfo.No,
   })
@@ -289,18 +312,20 @@ export class BaseEcpayDto {
   NeedExtraPaidInfo?: BaseEcpayNeedExtraPaidInfo;
 
   @ApiPropertyOptional({
-    description: `隱藏付款方式
-    當付款方式 [ChoosePayment] 為 ALL 時，可隱藏不需要的付款方式，多筆請以井號分隔 (#)。
+    description: `隱藏付款方式<br>
+    當付款方式 [ChoosePayment] 為 ALL 時，可隱藏不需要的付款方式，多筆請以井號分隔 (#)。<br>
     可用的參數值：
-    Credit：信用卡
-    ApplePay：Apple Pay
-    WebATM：網路ATM
-    ATM：自動櫃員機
-    CVS：超商代碼
-    BARCODE：超商條碼
-    TWQR : 行動支付
-    BNPL：裕富無卡分期
-    WeiXin：微信支付
+    <ul>
+    <li>Credit：信用卡</li>
+    <li>ApplePay：Apple Pay</li>
+    <li>WebATM：網路ATM</li>
+    <li>ATM：自動櫃員機</li>
+    <li>CVS：超商代碼</li>
+    <li>BARCODE：超商條碼</li>
+    <li>TWQR：行動支付</li>
+    <li>BNPL：裕富無卡分期</li>
+    <li>WeiXin：微信支付</li>
+    </ul>
     注意事項：綠界付款方式會不斷增加及調整，為避免因新付款方式造成接收付款結果通知失敗，建議串接時付款方式 [ChoosePayment] 固定指定付款方式。`,
     example: 'WebATM#ATM',
   })
@@ -310,7 +335,7 @@ export class BaseEcpayDto {
   IgnorePayment?: string;
 
   @ApiPropertyOptional({
-    description: `特約合作平台商代號
+    description: `特約合作平台商代號<br>
     為專案合作的平台商使用。`,
     example: 'PLT123',
   })
@@ -320,7 +345,7 @@ export class BaseEcpayDto {
   PlatformID?: string;
 
   @ApiPropertyOptional({
-    description: `自訂名稱欄位 1
+    description: `自訂名稱欄位 1<br>
     提供廠商使用記錄客製化欄位。`,
     example: 'Custom1',
   })
@@ -331,7 +356,7 @@ export class BaseEcpayDto {
   CustomField1?: string;
 
   @ApiPropertyOptional({
-    description: `自訂名稱欄位 2
+    description: `自訂名稱欄位 2<br>
     提供廠商使用記錄客製化欄位。`,
     example: 'Custom2',
   })
@@ -342,7 +367,7 @@ export class BaseEcpayDto {
   CustomField2?: string;
 
   @ApiPropertyOptional({
-    description: `自訂名稱欄位 3
+    description: `自訂名稱欄位 3<br>
     提供廠商使用記錄客製化欄位。`,
     example: 'Custom3',
   })
@@ -353,7 +378,7 @@ export class BaseEcpayDto {
   CustomField3?: string;
 
   @ApiPropertyOptional({
-    description: `自訂名稱欄位 4
+    description: `自訂名稱欄位 4<br>
     提供廠商使用記錄客製化欄位。`,
     example: 'Custom4',
   })
@@ -364,12 +389,14 @@ export class BaseEcpayDto {
   CustomField4?: string;
 
   @ApiPropertyOptional({
-    description: `語系設定
+    description: `語系設定<br>
     預設語系為中文，若要變更語系參數值請帶：
-    ENG：英語
-    KOR：韓語
-    JPN：日語
-    CHI：簡體中文`,
+    <ul>
+    <li>ENG：英語</li>
+    <li>KOR：韓語</li>
+    <li>JPN：日語</li>
+    <li>CHI：簡體中文</li>
+    </ul>`,
     enum: BaseEcpayLanguage,
     example: BaseEcpayLanguage.ENG,
   })
