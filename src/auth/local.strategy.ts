e35454cb2369f45d40a75cc82c78f9fd66c4544e
YuanShuoHsu <ook1725@gmail.com>
2025-08-04 22:14:53 +0800

    feat: 更新 LocalStrategy，新增 passwordField 屬性以支持密碼驗證，提升認證邏輯的一致性與可讀性。

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -8,10 +8,10 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
-    // super({
-    //   usernameField: 'email',
-    // });
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
   }
 
   async validate(
